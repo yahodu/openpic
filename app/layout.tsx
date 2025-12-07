f87600aae66633ee444ef9353faf4e88cdca25cd
Ashish Sam T George <ashishsamtgeorge@mca.ajce.in>
2025-12-08 03:03:59 +0530

    feat: implement sending the client message as email

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <FloatingHeader />
-
         {children}
       </body>
     </html>
